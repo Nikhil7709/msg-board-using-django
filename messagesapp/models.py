5d69a82a6340044904d1d90d130393dff099724d
Nikhil7709 <jadenik13@gmail.com>
2025-06-28 20:34:46 +0530

    feat | Added Message model

--- messagesapp/models.py
+++ messagesapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, 
+        related_name='user_messages'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.user.email[:15]}: {self.content[:30]}'
