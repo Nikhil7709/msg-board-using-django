089b8e5c8523cb67c4310c64f2a22fc39fbc961c
Nikhil7709 <jadenik13@gmail.com>
2025-06-28 20:33:29 +0530

    feat | Created User models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Custom manager for User model.
+    """
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    """
+    Custom User model that uses email as the unique identifier.
+    """
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
