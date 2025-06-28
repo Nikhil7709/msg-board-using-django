25ca51db514c405f7145df75730fa557a424517d
Nikhil7709 <jadenik13@gmail.com>
2025-06-29 00:00:23 +0530

    feat | Introduce MessageSerializer | Introduce notify_users_new_message | Created MessageListCreateAPIView | Register URL for MessageListCreateAPIView

--- messagesapp/urls.py
+++ messagesapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from messagesapp.views import MessageListCreateAPIView
 
 urlpatterns = [
-
+    path('', MessageListCreateAPIView.as_view(), name='message-list-create'),
 ]
+
--- messagesapp/views.py
+++ messagesapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from messagesapp.models import Message
+from rest_framework import permissions
 # Create your views here.
+from messagesapp.serializers import MessageSerializer
+
+class MessageListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
